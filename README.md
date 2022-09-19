5e01522364b3c394433c98a2d3b67cce9372c4c1
dufrane <dmitriy.vasilenko@gmail.com>
2022-09-19 21:30:29 +0400

    add weather view

---
+++ README.md
@@ -0,0 +1,29 @@
+![DV](Documentation/backGround.png)
+
+
+# WorkOut ✓
+
+
+## What skills I used
+
+* integrated third party libraries in app using Cocoapods.
+* create, update, save and delete data in Realm.
+* made queries and sort the database.
+* worked with UITableViews and how to set their data sources and delegates.
+* create custom cells
+* make http requests 
+* decode JSON
+* all UI written programmaticaly 
+
+
+
+# To bild:
+- clone the repo
+- install cocoaPods
+- install Realm
+- launch WorkOut.xcworkspace
+
+
+![DV](Documentation/screen_1.png)
+![DV](Documentation/screen_2.png)
+![DV](Documentation/screen_3.png)
---
+++ workOut/workOut/Extensions/UIKit/UIStackView + Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIStackView + Extensions.swift
+//  workOut
+//
+//  Created by Dmytro Vasylenko on 19.09.2022.
+//
+
+import UIKit
+//let stackView = UIStackView()
+//stackView.addArrangedSubview(YourView)
+//stackView.addArrangedSubview(YourView2)
+//stackView.axis = .horizontal
+//stackView.spacing = 10
+//stackView.translatesAutoresizingMaskIntoConstraints = false
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
