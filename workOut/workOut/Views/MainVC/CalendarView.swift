e257e44b9ff1a6930b1a9cc0f85fee3b93725700
dufrane <dmitriy.vasilenko@gmail.com>
2022-09-21 21:01:33 +0400

    add collection view

--- workOut/workOut/Views/MainVC/CalendarView.swift
+++ workOut/workOut/Views/MainVC/CalendarView.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class CalendarView: UIView {
     
+    private let collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 3
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .none
+        return collectionView
+    }()
+    
+    private let idCalendarCell = "idCalendarCell"
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupViews()
         setConstraints()
+        setDelegates()
     }
     
     required init?(coder: NSCoder) {
@@ -25,6 +36,42 @@ class CalendarView: UIView {
         backgroundColor = #colorLiteral(red: 0.2156862745, green: 0.5529411765, blue: 0.4901960784, alpha: 1)
         layer.cornerRadius = 10
         translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: idCalendarCell )
+    }
+    
+    private func setDelegates() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+
+extension CalendarView: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: idCalendarCell, for: indexPath)
+        return cell
+    }
+}
+
+extension CalendarView: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Tap cell")
+    }
+}
+
+extension CalendarView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width / 8,
+               height: collectionView.frame.height)
     }
 }
 
@@ -32,6 +79,10 @@ extension CalendarView {
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
             
         ])
     }
