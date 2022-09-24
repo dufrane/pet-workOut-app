9ae1c392f8ba3275d112866b5fd86446e3d2ca69
dufrane <dmitriy.vasilenko@gmail.com>
2022-09-24 17:59:18 +0400

    add newWorkout views and realm

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '14.0'
+
+target 'workOut' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+	
+
+  # Pods for workOut
+
+  pod 'RealmSwift', '~>10'
+
+end
--- workOut/Extensions/UIKit/UILabel + Extensions.swift
+++ workOut/Extensions/UIKit/UILabel + Extensions.swift
@@ -10,11 +10,20 @@ import UIKit
 extension UILabel {
     convenience init(text: String = "") {
         self.init()
-        
         self.text = text
         self.font = .robotoMedium14()
         self.textColor = .specialLightBrown
         self.adjustsFontSizeToFitWidth = true
         self.translatesAutoresizingMaskIntoConstraints = false
     }
+    
+    convenience init(text: String = "", font: UIFont?, textColor: UIColor) {
+        self.init()
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.adjustsFontSizeToFitWidth = true
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
 }
+
